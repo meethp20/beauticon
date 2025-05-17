551beab58324cb571bd0cbd6fb08ee22073efba4
Meeth Patel <meeth2060@gmail.com>
2025-05-17 16:40:44 +0530

    coolpic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card({title, description,imageUrl, ...props}) {
   return (
     <div 
-    className='border-4 border-box shadow-lg shadow-pink-500/50 rounded-3xl flex hover:scale-105 transition-all duration-500 w-[1000px] bg-gradient-to-r from-pink-700 to-30% '
+    className='border-4 border-box shadow-lg shadow-pink-500/50 rounded-3xl flex drop-shadow-lg  hover:scale-105 transition-all duration-500 w-[1000px] bg-gradient-to-r from-pink-700 to-30% '
     
     >
            <div className='text-black w-1/2 text-center flex items-center justify-center flex-col p-4'>
@@ -14,8 +14,8 @@ function Card({title, description,imageUrl, ...props}) {
                 {description}
               </p>
            </div>
-           <div className='w-1/2  justify-end h-[300px] object-cover object-top '>
-                <img src={imageUrl} alt={title} className=' right-0 top-0 w-full h-full rounded-3xl object-cover   shadow-lg'
+           <div className='w-1/2  justify-end h-[300px] object-cover bg-top '>
+                <img src={imageUrl} alt={title} className=' right-0 top-0 drop-shadow-lg  w-full mask-b-from-50% mask-radial-[50%_90%] mask-radial-from-80% h-full rounded-3xl object-cover   shadow-lg'
                 onError={(e)=>{e.target.src = '';e.target.onerron=null;}} />
            </div>
     </div>
