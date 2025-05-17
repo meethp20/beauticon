5a73b652922fccd393b7e20c6314dd6a28cf82c6
Meeth Patel <meeth2060@gmail.com>
2025-05-17 14:58:30 +0530

    updated card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card({title, description,imageUrl, ...props}) {
   return (
     <div 
-    className='border-4 shadow-lg shadow-pink-500/50 rounded-3xl flex hover:scale-105 transition-all duration-400 w-[1000px]'
+    className='border-4 border-box shadow-lg shadow-pink-500/50 rounded-3xl flex hover:scale-105 transition-all duration-500 w-[1000px] bg-gradient-to-r from-pink-700 to-30% '
     
     >
            <div className='text-black w-1/2 text-center flex items-center justify-center flex-col p-4'>
@@ -14,8 +14,8 @@ function Card({title, description,imageUrl, ...props}) {
                 {description}
               </p>
            </div>
-           <div className='w-1/2  justify-end h-[300px] object-cover '>
-                <img src={imageUrl} alt={title} className=' right-0 top-0 w-full h-full rounded-lg object-cover  shadow-lg'
+           <div className='w-1/2  justify-end h-[300px] object-cover object-top '>
+                <img src={imageUrl} alt={title} className=' right-0 top-0 w-full h-full rounded-3xl object-cover   shadow-lg'
                 onError={(e)=>{e.target.src = '';e.target.onerron=null;}} />
            </div>
     </div>
